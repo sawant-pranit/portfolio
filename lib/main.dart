312317b6bf820858735dde441b045d75602b771b
ez96159 <Ibm@bell2993>
2023-10-06 19:41:39 +0530

    initial project setup changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/view/splash/splash_view.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/res/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,26 +15,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: bgColor,
         useMaterial3: true,
+        textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white,)
+            .copyWith(
+          bodyText1: const TextStyle(color: bodyTextColor),
+          bodyText2: const TextStyle(color: bodyTextColor),
+        ),
       ),
-      home: const PortfolioWebsite()/*MyHomePage(title: 'Flutter Demo Home Page')*/,
+      home: const SplashView()/*MyHomePage(title: 'Flutter Demo Home Page')*/,
     );
   }
 }
@@ -169,14 +165,16 @@ class HeroSection extends StatelessWidget {
             end: Alignment.bottomRight,
           )
       ),
-      child: const Center(
-        child: Text(
-          'Your Name',
-          style: TextStyle(
-            fontSize: 48,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
+      child: const SizedBox(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(),
+            Text('Your Website'),
+            SizedBox(),
+          ],
         ),
       ),
     );
---
+++ lib/res/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+const primaryColor = Colors.white;
+const secondaryColor = Color(0xFF242430);
+const darkColor = Color(0xFF191923);
+const bodyTextColor = Color(0xFF8B8B8D);
+const bgColor = Color(0xFF000515);
+const defaultPadding = 20.0;
+
+class Strings {
+  static const String name = 'pranit sawant';
+
+  static const String shortDescription = """I possess expert-level proficiency in 
+  native Android development, showcasing a deep understanding of the Android 
+  platform and its intricacies. Furthermore, I possess intermediate-level skills 
+  in iOS and Flutter development, demonstrating a solid foundation in cross-platform 
+  mobile app development. Additionally, I have a beginner-level grasp of backend 
+  technologies like Ktor and Spring Boot, indicating a burgeoning aptitude for 
+  server-side development and API integration.""";
+
+
+}
