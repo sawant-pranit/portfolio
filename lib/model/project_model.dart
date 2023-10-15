9dbffc8a9308a12783df7dfebc1d58512d044902
ez96159 <Ibm@bell2993>
2023-10-15 21:40:21 +0530

    Added images of remaining android projects

--- lib/model/project_model.dart
+++ lib/model/project_model.dart
@@ -25,33 +25,34 @@ List<Project> projectList = [
       'assets/images/alarm.jpg',
       'https://github.com/Hamad-Anwar/Neumorphic-Analog-Clockify'),
   Project(
-      'CUI Aider',
+      'Loner',
       'CUI AIDER is an extraordinary application that brings together augmented reality (AR), a robust student portal, intuitive class management, and a comprehensive GPA system, providing an all-encompassing solution for students.Using AR technology, CUI AIDER offers an immersive experience, allowing users to explore the complete university campus virtually. Through AR, students can visualize buildings, navigate with interactive directions, and gain a better understanding of their surroundings.',
-      'assets/images/cui.png',
+      'images/loner.png',
       'https://github.com/Hamad-Anwar/'),
   Project(
-      'Flutter Music Streaming Application',
+      'City Center Mall',
       'A Flutter music streaming application that allows users to play audio files from local storage. The app features a beautiful neumorphic UI design and provides a smooth user experience for browsing and playing music.',
-      'assets/images/player.png',
+      'images/ccm.png',
       'https://github.com/Hamad-Anwar/Neumorphic-Music_Player-Flutter'),
   Project(
-      'Food Recipe App',
+      'Mall of Emirates',
       'This repository contains a Flutter implementation of a Food Recipe app with a captivating introduction section and impressive animations. It also features a signup and signin page with animations, elegantly presented on a bottom sheet.',
-      'assets/images/recipe.png',
+      'images/moe.png',
       'https://github.com/Hamad-Anwar/Food-Recipe-App-Flutter'),
   Project(
-      'Task Sync Pro',
+      'Jomblo',
       'Welcome to the Beautiful Task Scheduler App repository! This Flutter-based task management application combines elegant design with a robust backend, ensuring a seamless and organized task management experience. From stunning UI to real-time synchronization, this app has you covered.',
-      'assets/images/task.png',
+      'images/jomblo.png',
       'https://github.com/Hamad-Anwar/Task-Sync-Pro-Flutter'),
   Project(
-      'Flutter Chat Application with Firebase',
+      'Food Social',
       'Welcome to our innovative Flutter chat application! This feature-rich messaging platform allows users to connect and communicate seamlessly through text and images. The app is built using Flutter for the frontend and integrates with Firebase for backend services, including authentication, real-time database, and storage.',
-      'assets/images/chat.png',
+      'images/food_social.png',
       'https://github.com/Hamad-Anwar/Messenger-App-Backend-Firebase'),
   Project(
-      'Doctor Appointment Application',
-      'Introducing the extraordinary "Doctor Appointment System" - a state-of-the-art Flutter UI application that redefines healthcare accessibility and efficiency. Seamlessly crafted, this app empowers users to effortlessly select doctors based on categories, engage in smooth messaging, and access detailed profiles.',
-      'assets/images/doctor.png',
-      'https://github.com/Hamad-Anwar/Doctor-Appointment-Application-UI'),
+    'Gutrgoo',
+    'The newest Social Media platform, targeting micro contenting. Responsible for MVP design pattern with Repository pattern, Websocket client side implementation.',
+    'images/gutrgoo.png',
+    'https://github.com/Hamad-Anwar/Doctor-Appointment-Application-UI'
+  ),
 ];
