e9eacbf1e28c257273c8ff234494377b166f0149
pranit <pranitsawant01@gmail.com>
2023-10-19 22:57:33 +0530

    fixed navigation flow

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -12,9 +12,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MainView(pages: [
       const Introduction(),
+      const About(),
       ProjectsView(),
       Certifications(),
-      const About(),
     ]);
   }
 }
